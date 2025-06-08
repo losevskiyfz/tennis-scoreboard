5385f0093368c641f9e4e617bead7f9f66c4b919
Fyodor Losevskiy <losevskiyfz@gmail.com>
2025-06-09 01:29:28 +0300

    fix: fixed N+1

--- src/main/java/com/github/losevskiyfz/dao/MatchDaoImpl.java
+++ src/main/java/com/github/losevskiyfz/dao/MatchDaoImpl.java
@@ -18,30 +18,17 @@ public class MatchDaoImpl implements MatchDao {
 
     @Override
     public List<Match> findByMatchPaged(int pageNumber, int pageSize, String nameFilter, EntityManager em) {
-
-        String idQuery = """
-                    SELECT m.id FROM Match m
+        String hql = """
+                    SELECT m FROM Match m
+                    JOIN FETCH m.player1
+                    JOIN FETCH m.player2
                     WHERE m.player1.name LIKE :nameFilter OR m.player2.name LIKE :nameFilter
                     ORDER BY m.id DESC
                 """;
-
-        List<Integer> ids = em.createQuery(idQuery, Integer.class)
+        return em.createQuery(hql, Match.class)
                 .setParameter("nameFilter", "%" + nameFilter + "%")
                 .setFirstResult((pageNumber - 1) * pageSize)
                 .setMaxResults(pageSize)
-                .getResultList();
-
-        if (ids.isEmpty()) return List.of();
-
-        String fullQuery = """
-                    SELECT DISTINCT m FROM Match m
-                    JOIN FETCH m.player1
-                    JOIN FETCH m.player2
-                    WHERE m.id IN :ids
-                    ORDER BY m.id
-                """;
-        return em.createQuery(fullQuery, Match.class)
-                .setParameter("ids", ids)
                 .getResultList();
     }
 }
