61234b291911f3b1f95f601f04855ef9795d7860
Fyodor Losevskiy <losevskiyfz@gmail.com>
2025-06-08 22:19:13 +0300

    fix: fixed N+1

--- src/main/java/com/github/losevskiyfz/dao/MatchDaoImpl.java
+++ src/main/java/com/github/losevskiyfz/dao/MatchDaoImpl.java
@@ -6,7 +6,7 @@ import jakarta.persistence.EntityManager;
 import java.util.List;
 import java.util.logging.Logger;
 
-public class MatchDaoImpl implements MatchDao{
+public class MatchDaoImpl implements MatchDao {
     private static final Logger LOG = Logger.getLogger(MatchDaoImpl.class.getName());
 
     @Override
@@ -18,11 +18,30 @@ public class MatchDaoImpl implements MatchDao{
 
     @Override
     public List<Match> findByMatchPaged(int pageNumber, int pageSize, String nameFilter, EntityManager em) {
-        String hql = "FROM Match m WHERE m.player1.name LIKE :nameFilter OR m.player2.name LIKE :nameFilter ORDER BY m.ID";
-        return em.createQuery(hql, Match.class)
+
+        String idQuery = """
+                    SELECT m.id FROM Match m
+                    WHERE m.player1.name LIKE :nameFilter OR m.player2.name LIKE :nameFilter
+                    ORDER BY m.id DESC
+                """;
+
+        List<Integer> ids = em.createQuery(idQuery, Integer.class)
                 .setParameter("nameFilter", "%" + nameFilter + "%")
                 .setFirstResult((pageNumber - 1) * pageSize)
                 .setMaxResults(pageSize)
+                .getResultList();
+
+        if (ids.isEmpty()) return List.of();
+
+        String fullQuery = """
+                    SELECT DISTINCT m FROM Match m
+                    JOIN FETCH m.player1
+                    JOIN FETCH m.player2
+                    WHERE m.id IN :ids
+                    ORDER BY m.id
+                """;
+        return em.createQuery(fullQuery, Match.class)
+                .setParameter("ids", ids)
                 .getResultList();
     }
 }
